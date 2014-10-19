4c26e5e3b32aa9133f99d2b73b7aa21a229c7819
uchihashi_k <uchihashi.kenshi@gmail.com>
2014-10-19 15:29:09 +0900

    init

---
+++ ITP1_1_GettingStarted/pro3_1.cpp
@@ -0,0 +1,13 @@
+#include<cstdio>
+
+int add(int, int);
+
+int main() {
+	int S = add(7, 3);
+
+	return 0;
+}
+
+int add(int a, int b) {
+	return a + b;
+}
---
+++ ITP1_1_GettingStarted/pro4_1.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main () {
+	int a = 5, b = 8, t;
+
+	t = a;
+	a = b;
+	b = t;
+
+	return 0;
+}
